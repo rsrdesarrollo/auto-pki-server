c7121fd01619113a6e67c0ecda728bbf0b7669c8
Raúl Sampedro <rsrdesarrollo@gmail.com>
2016-04-11 18:01:17 +0200

    add config sample. add middleware for pkcs10

---
+++ controller/est_controller_cfssl.js
@@ -0,0 +1,3 @@
+/**
+ * Created by raul on 4/6/16.
+ */
---
+++ middleware/pkcs10.js
@@ -0,0 +1,51 @@
+const forge = require('node-forge');
+const is = require('type-is');
+const content_type = require('content-type');
+
+var debug = require('debug')('middleware:pkcs10');
+
+function get_charset(req){
+    try {
+        return contentType.parse(req).parameters.charset.toLowerCase()
+    } catch (e) {
+        return undefined
+    }
+}
+
+function pkcs10_request_parser(req, res, next){
+    if(req.method !== "POST"){
+        return debug('skip not POST'), next()
+    }
+
+    if(!is(req,"application/pkcs10")){
+        return debug('skip wrong content type'), next()
+    }
+
+    if(req._body){
+        return debug('body already parsed'), next();
+    }
+
+    // skip requests without bodies
+    if (!is.hasBody(req)) {
+        return debug('skip empty body'), next()
+    }
+
+    var charset = get_charset(req) || 'utf-8';
+
+    var rawBody = '';
+    req.setEncoding(charset);
+
+    req.on('data', function(chunk) {
+        rawBody += chunk;
+    });
+
+    req.on('end', function() {
+        // flag as parsed
+        req._body = true
+        req.csr = forge.pki.certificationRequestFromPem(rawBody);
+
+        next();
+    });
+}
+
+module.exports = pkcs10_request_parser;
